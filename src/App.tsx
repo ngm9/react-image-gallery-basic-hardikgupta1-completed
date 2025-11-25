69cb88dea4d238d3f0fe6c2a6c52414453f3bafb
Naman Muley <naman.g.muley@gmail.com>
2025-11-25 06:02:20 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ImageGallery from './components/ImageGallery';
+
+function App() {
+  return (
+    <div>
+      <h1>Utkrusht Image Gallery</h1>
+      <ImageGallery />
+    </div>
+  );
+}
+
+export default App;
